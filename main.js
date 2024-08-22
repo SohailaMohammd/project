9912b162cfbd545a7a419efb76a4d01be31ec8cd
SohailaMohammd <160627240+SohailaMohammd@users.noreply.github.com>
2024-08-22 04:20:30 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function showLinks() {
+    const links = document.querySelector(".links");
+    links.style.display = 'flex';
+}
+function hideLinks() {
+    const links = document.querySelector(".links");
+    links.style.display = 'none';
+}
